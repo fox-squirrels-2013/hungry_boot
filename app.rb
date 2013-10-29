af88d3883836b56050979abcaacc4cd496a34621
James & Strand <strand@devbootcamp.com>
2013-10-29 11:42:16 -0700

    Create Restaurant using TDD.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'active_record'
+require './app/models/restaurant'
 
 ActiveRecord::Base.establish_connection(adapter: 'postgresql',
                                         database: 'sinatra_skeleton_dev')
