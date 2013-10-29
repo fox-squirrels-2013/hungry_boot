b70fdc0695b596300eff6f31ff036a9b1e9f838a
James & Strand <strand@devbootcamp.com>
2013-10-29 11:19:26 -0700

    remove elephant model from skeleton

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 require 'active_record'
-require_relative './app/models/elephant'
 
 ActiveRecord::Base.establish_connection(adapter: 'postgresql',
                                         database: 'sinatra_skeleton_dev')
