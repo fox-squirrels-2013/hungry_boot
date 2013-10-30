f8a1b67ab17f5eb6806c969374dc92e58066bd70
James Bond <james@devbootcamp.com>
2013-10-30 00:49:32 -0700

    oops! bad James!! broke the tests and didn't notice, because I wasn't diligently running rspec first.

--- spec/features/guest_may_create_a_restaurant_spec.rb
+++ spec/features/guest_may_create_a_restaurant_spec.rb
@@ -7,32 +7,6 @@ feature "Creating a restaurant" do
     fill_in "location", with: "Down Pine"
     click_on "Record Restaurant!"
 
-    expect(page).to have_content("The last restaurant recorded is Pasilla!")
+    expect(page).to have_content("You added a new restaurant: Pasilla")
   end
 end
-
-# feature "Claiming a lightning talk" do
-#   let(:user_attributes) { random_user_attributes }
-#   let!(:user) { User.create(user_attributes) }
-#   let!(:talk) { create_random_unclaimed_talk }
-#   let!(:claimed_talk) { create_random_claimed_talk }
-
-#   scenario "Registered user may claim a lightning talk" do
-#     login(user_attributes)
-#     visit '/'
-#     within "#talk-#{talk.id}" do
-#       click_on "I'll do it!"
-#     end
-
-#     expect(page).to have_content("#{talk.name} - Presented By #{user.name}")
-#   end
-
-#   scenario "Claimed talks are not listed under suggested talks" do
-#     login(user_attributes)
-#     visit '/'
-
-#     within '#suggested-talks' do
-#       expect(page).not_to have_selector("#talk-#{claimed_talk.id}")
-#     end
-#   end
-# end
