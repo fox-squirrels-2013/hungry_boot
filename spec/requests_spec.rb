5089367f7d428153a297f41bba40951b059230f5
Strand McCutchen <strand@bettermistak.es>
2013-10-30 10:55:32 -0700

    Request spec demonstrates non-save when no location is specified.

--- spec/requests_spec.rb
+++ spec/requests_spec.rb
@@ -6,4 +6,9 @@ describe "web requests" do
                                 'location' => "Down on Kearny"} }
     expect {post '/restaurants', params}.to change {Restaurant.all.length}.by(1)
   end
+
+  it "does not create restaurant without a location" do
+    params = { 'restaurant' => {'name' => "Sushiritto" } }
+    expect {post '/restaurants', params}.to change {Restaurant.all.length}.by(0)
+  end
 end
